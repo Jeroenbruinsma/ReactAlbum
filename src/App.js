d2ad194779233f5ec24886b0c2df19bf86fec0da
Jeroen Bruinsma <jeroenbruinsma@gmail.com>
2019-06-17 11:40:11 +0200

    told app to render AlbumListContainer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import './App.css';
 import {Provider} from 'react-redux'
 import store from './store'
+import AlbumsListContainer from './components/AlbumsListContainer';
 
 
 class App extends React.Component {
@@ -12,6 +13,7 @@ class App extends React.Component {
       <Provider store={store}>
         <div className="App">
           <header className="App-header">
+          <AlbumsListContainer/>
 
 
           </header>
